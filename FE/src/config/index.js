2e6d572f70d18a5d058db04881d2cc57ff00bbd6
Phuong <dongnam250@gmail.com>
2025-10-19 19:37:57 +0700

    sign-up-and-sign-in

---
+++ FE/src/config/index.js
@@ -0,0 +1,40 @@
+export const resgisterFormControls = [
+  {
+    name: "userName",
+    label: "User Name",
+    type: "text",
+    placeholder: "Enter your username",
+    componentType: "input",
+  },
+  {
+    name: "email",
+    label: "Email",
+    type: "text",
+    placeholder: "Enter your email",
+    componentType: "email",
+  },
+  {
+    name: "password",
+    label: "Password",
+    type: "password",
+    placeholder: "Enter your password",
+    componentType: "password",
+  },
+];
+
+export const loginFormControls = [
+  {
+    name: "email",
+    label: "Email",
+    type: "text",
+    placeholder: "Enter your email",
+    componentType: "email",
+  },
+  {
+    name: "password",
+    label: "Password",
+    type: "password",
+    placeholder: "Enter your password",
+    componentType: "password",
+  },
+];
